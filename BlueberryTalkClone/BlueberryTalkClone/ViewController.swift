656058f7e6c39ff56eac0d526cfee1b29246cf64
woong <workplayhard1@naver.com>
2023-05-23 01:47:04 +0900

    build: BlueBerryTalk 프로젝트 생성 및 기기 및 타겟설정

---
+++ BlueberryTalkClone/BlueberryTalkClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BlueberryTalkClone
+//
+//  Created by 서현웅 on 2023/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
