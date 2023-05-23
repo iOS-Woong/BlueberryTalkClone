600706fae521af340dc82292614a7e8c82667214
woong <workplayhard1@naver.com>
2023-05-23 15:45:23 +0900

    feat: 로그인창에서 사용자의 정보를 받아올 UserInfo 타입의 구현

---
+++ BlueberryTalkClone/BlueberryTalkClone/UserInfo.swift
@@ -0,0 +1,19 @@
+//
+//  UserInfo.swift
+//  BlueberryTalkClone
+//
+//  Created by 서현웅 on 2023/05/23.
+//
+
+import Foundation
+
+struct UserInfo {
+    let name: String
+    let description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+    
+}
